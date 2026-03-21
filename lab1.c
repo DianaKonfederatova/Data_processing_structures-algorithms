bceb23ec14e195a20a68edc545f4a4a32b1aea50
Diana <konfederatovadianochka@yandex.ru>
2026-03-21 12:28:35 +0700

    Сделала SelectSort и подсчет контрольной суммы

--- lab1.c
+++ lab1.c
@@ -1,43 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//пока в процессе, не продуман момент сохранения прежнего значения
-//стоит подумать над началом поиска элемента
-//сразу не менять данные лучше
+
 void SelectSort(int massive_orig[], int n){
     
     for (int i = 0; i<n; i++){
-        int min_el = massive_orig[0];
 
-        for(int j=1; j<n; j++){
+        int min_index = i;
+        
 
-            int temp = massive_orig[j];
+        for(int j=i+1; j<n; j++){
 
-            if(min_el>temp){
-                
-                min_el = temp;
-                massive_orig[i] = massive_orig[j];
+            if(massive_orig[min_index]>massive_orig[j]){
+
+                min_index = j; 
+
             }
-        }        
+        }
+
+        int temp = massive_orig[i];
+        massive_orig[i]=massive_orig[min_index];
+        massive_orig[min_index] = temp;       
+        
+         
     }
 
+    int control_sum_massive_sort = 0;
     for (int d=0; d<n; d++){
+        control_sum_massive_sort += massive_orig[d];
         printf("%d", massive_orig[d]);
     }
-    
+    printf("\n");
+    printf("Контрольная сумма после сортировки:\n");
+    printf("%d\n", control_sum_massive_sort);
+ 
 }
 
 void Random_massive(int massive[], int size, int min, int max){
     for (int i=0; i<size; i++){
         massive[i] = rand() % (max - min + 1) + min;
     }
+
+    int control_sum_orig = 0;
+    for (int d=0; d<size; d++){
+        control_sum_orig += massive[d];
+    }
+    printf("%d\n", control_sum_orig);
     
     printf("Исходный массив:\n");
     for (int j=0; j<size; j++){
         printf("%d", massive[j]);
     }
     printf("\n");
-
+  
 }
 
 int main(){
@@ -52,11 +67,10 @@ int main(){
     scanf("%d", &max);
     int massive[size];
 
+    printf("Контрольная сумма до сортировки:\n");
     Random_massive(massive, size, min, max);
-
+    
     printf("Массив после SelectSort: \n");
     SelectSort(massive, size);
-
-    
     
 }
