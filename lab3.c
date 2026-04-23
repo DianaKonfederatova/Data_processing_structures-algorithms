dba78123b992e7aa3b7f2682029d7b5b6516a0c4
Diana <konfederatovadianochka@yandex.ru>
2026-04-23 12:44:21 +0700

    Добален вывод характеристик отсортированных массивов, подсчет времени сортировки массива, конечный вариант работы

--- lab3.c
+++ lab3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
-
+#include <string.h>
+#include <stdlib.h>
 
 
 int count_series(int massive[], int size){
@@ -48,8 +49,8 @@ void Decrease_massive(int massive[], int size, int max){
 }
 
 void ShakerSort(int massive[], int n, int* cs, int* cc){
-    int cc = 0;
-    int cs = 0;
+    *cc = 0; 
+    *cs = 0;
     int L = 0;
     int R = n-1;
     while(L<R){
@@ -79,6 +80,82 @@ void ShakerSort(int massive[], int n, int* cs, int* cc){
     }
 }
 
+void labor_intensity(const char* name, int n) {
+    if (strcmp(name, "ВОЗРАСТАЮЩИЙ МАССИВ") == 0) {
+        int C = n-1;
+        int M = 0;
+        printf("Теоретическое количество сравнений: %d\n", C);
+        printf("Теоретическое количество пересылок: %d\n", M);
+        printf("Теоретическая трудоемкость: %d\n", C+M);
+    }
+
+    if (strcmp(name, "СЛУЧАЙНЫЙ МАССИВ") == 0) {
+        int C = ((n*n)-n)/4;
+        int M = (3*((n*n)-n))/4;
+        printf("Теоретическое количество сравнений: %d\n", C);
+        printf("Теоретическое количество пересылок: %d\n", M);
+        printf("Теоретическая трудоемкость: %d\n", C+M);
+    }
+
+    if (strcmp(name, "УБЫВАЮЩИЙ МАССИВ") == 0) {
+        int C = ((n*n)-n)/2;
+        int M = (3*((n*n)-n))/2;
+        printf("Теоретическое количество сравнений: %d\n", C);
+        printf("Теоретическое количество пересылок: %d\n", M);
+        printf("Теоретическая трудоемкость: %d\n", C+M);
+    }
+}
+
+void Test(const char* name, int massive[], int size){
+    int counter_shipment = 0;   
+    int counter_comparison = 0;
+    printf("==============================");
+    printf("%s\n", name);
+    printf("Массив до сортировки:\n");
+    for (int i = 0; i<size; i++){
+        printf("%d ", massive[i]);
+    }
+
+    printf("\n");
+
+    int control_sum_befor = control_sum(massive, size);
+    printf("Контрольная сумма до сортировки: %d\n", control_sum_befor);
+    int series_befor = count_series(massive, size);
+    printf("Количество серий до сортировки: %d\n", series_befor);
+    printf("\n\n");
+
+    clock_t start_time = clock();
+    ShakerSort(massive, size, &counter_shipment, &counter_comparison);
+    clock_t end_time = clock();
+
+    double time_spent = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+    printf("--------------------------------------------------\n");
+    printf("После сортировки:\n");
+    for (int i = 0; i<size; i++){
+        printf("%d ", massive[i]);
+    }
+
+    printf("\n");
+
+    int control_sum_after = control_sum(massive, size);
+    printf("Контрольная сумма после сортировки: %d\n", control_sum_after);
+    int series_after = count_series(massive, size);
+    printf("Количество серий после сортировки: %d\n", series_after);
+    printf("\n\n");
+
+    printf("ХАРАКТЕРИСТИКИ\n");
+    printf("Фактическое количество сравнений: %d\n", counter_comparison);
+    printf("Фактическое количество пересылок: %d\n", counter_shipment);
+    int T_fact = counter_comparison + counter_shipment;
+    printf("Трудоемкость фактическая: %d", T_fact);
+
+    printf("\n\n");
+    labor_intensity(name, size);
+    printf("Время выполнения: %.6f сек (или %.3f мс)\n", time_spent, time_spent * 1000);
+    printf("\n\n");
+}
+
 
 int main(){
     srand(time(NULL));
@@ -96,7 +173,15 @@ int main(){
     int massive[size];
     printf("\n\n");
 
+    Increase_massive(massive, size, min);
+    Test("ВОЗРАСТАЮЩИЙ МАССИВ", massive, size);
 
+    
+    Decrease_massive(massive, size, max);
+    Test("УБЫВАЮЩИЙ МАССИВ", massive, size);
 
+    
+    Random_massive(massive, size, min, max);
+    Test("СЛУЧАЙНЫЙ МАССИВ", massive, size);
 
 }
